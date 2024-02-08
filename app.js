5dfb0ddb4fa546db997c159f5679e703a8ffd392
Ramakanth Muthyala <ramakanthmuthyala008@gmail.com>
2024-02-08 15:44:30 -0800

    key-code

---
+++ app.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+window.addEventListener("keydown", (event) => {
+  container.innerHTML = `
+    <div class='key'>
+${event.key === " " ? "space" : event.key}
+<small>event.key</small>
+    </div>
+     <div class='key'>
+     ${event.keyCode}
+     <small>event.keyCode</small>
+         </div>
+         <div class='key'>
+     ${event.code}
+     <small>event.code</small>
+         </div>`;
+});
